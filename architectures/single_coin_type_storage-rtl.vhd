f2943c4225d423721085b3f21ddf0a691ea2d8f8
Khalil Acheche <achechekhalil@hotmail.fr>
2020-05-10 19:19:53 +0200

    frf

--- architectures/single_coin_type_storage-rtl.vhd
+++ architectures/single_coin_type_storage-rtl.vhd
@@ -4,7 +4,7 @@ ARCHITECTURE rtl OF single_coin_type_storage IS
   SIGNAL ovf_inc_dec_unit,enable_sum_reg :std_logic;
 BEGIN
 
-inc_dec_unit : PROCESS(add_coin , rem_coin, s_counter_reg) IS
+inc_dec_unit : PROCESS(add_coin , rem_coin, s_counter_reg,s_sum_reg) IS
 BEGIN
     IF(add_coin='1') THEN   -- When both add_coin and rem_coin are 1, addition
       IF (s_counter_reg= to_unsigned( 31 , 5 )) THEN
@@ -12,6 +12,7 @@ BEGIN
       ELSE
         ovf_inc_dec_unit<='0';
         s_inc_dec_unit <= s_counter_reg + 1;
+        s_add_sub_unit <= float_to_fixed(fixed_to_float(std_logic_vector(s_sum_reg)) + fixed_to_float(std_logic_vector(COIN_VAL)));
       END IF;
     ELSIF(rem_coin='1') THEN
       IF (s_counter_reg= to_unsigned( 0 , 5 )) THEN
@@ -19,14 +20,15 @@ BEGIN
       ELSE
         ovf_inc_dec_unit<='0';
         s_inc_dec_unit <= s_counter_reg - 1;
+        s_add_sub_unit <= float_to_fixed(fixed_to_float(std_logic_vector(s_sum_reg)) - fixed_to_float(std_logic_vector(COIN_VAL)));
       END IF;
     ELSE ovf_inc_dec_unit<='0';
     END IF;
 END PROCESS;
 e_counter_reg<= to_unsigned(INIT_COUNT,5) WHEN reset='1' ELSE s_inc_dec_unit;
-counter_reg : PROCESS(clk,en,e_counter_reg)
+counter_reg : PROCESS(clk,enable_sum_reg,e_counter_reg)
 BEGIN
-    IF(en='1') THEN
+    IF(enable_sum_reg='1') THEN
       IF(rising_edge(clk)) THEN
         s_counter_reg<=e_counter_reg;
       END IF;
@@ -35,20 +37,12 @@ END PROCESS;
 
 count <= std_logic_vector(to_unsigned(0,5)) WHEN en = '0' ELSE std_logic_vector(s_counter_reg);
 fault <=en AND ovf_inc_dec_unit;
-add_sub_unit : PROCESS(add_coin,rem_coin,s_sum_reg)
-BEGIN
-  IF(add_coin='1') THEN   -- When both add_coin and rem_coin are 1, addition
-      s_add_sub_unit <= float_to_fixed(fixed_to_float(std_logic_vector(s_sum_reg)) + fixed_to_float(std_logic_vector(COIN_VAL)));
-  ELSIF(rem_coin='1') THEN
-      s_add_sub_unit <= float_to_fixed(fixed_to_float(std_logic_vector(s_sum_reg)) - fixed_to_float(std_logic_vector(COIN_VAL)));
-  END IF;
-END PROCESS;
 e_sum_reg<= INIT_SUM WHEN reset='1' ELSE s_add_sub_unit;
-enable_sum_reg<= en AND NOT ovf_inc_dec_unit;
+enable_sum_reg<= en AND (NOT ovf_inc_dec_unit);
 sum_reg : PROCESS(clk,enable_sum_reg,e_sum_reg)
 BEGIN
-    IF(rising_edge(clk)) THEN
-      IF(enable_sum_reg ='1') THEN
+    IF(enable_sum_reg ='1') THEN
+      IF(rising_edge(clk)) THEN
         s_sum_reg<=e_sum_reg;
       END IF;
     END IF;
